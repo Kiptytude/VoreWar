0f04bbbaf148d978579a547297c6cc20b6dbc582
Flame Valxsarion <136280114+Flame-Valxsarion@users.noreply.github.com>
2023-08-31 23:14:18 -0600

    Aabayx Invalid customizer button fix

--- Assets/Scripts/Races/Graphics/MainRaces/Aabayx.cs
+++ Assets/Scripts/Races/Graphics/MainRaces/Aabayx.cs
@@ -17,6 +17,7 @@ class Aabayx : DefaultRaceData
         MouthTypes = 0;
         TailTypes = 16;
         SpecialAccessoryCount = 0;
+        clothingColors = 0;
         AccessoryColors = ColorPaletteMap.GetPaletteCount(ColorPaletteMap.SwapType.AabayxSkin); // Head color
         SkinColors = ColorPaletteMap.GetPaletteCount(ColorPaletteMap.SwapType.AabayxSkin);
 
@@ -48,7 +49,7 @@ class Aabayx : DefaultRaceData
         BreastShadow = null;
         Dick = new SpriteExtraInfo(11, DickSprite, null, (s) => ColorPaletteMap.GetPalette(ColorPaletteMap.SwapType.AabayxSkin, s.Unit.SkinColor));
         Balls = new SpriteExtraInfo(10, BallsSprite, null, (s) => ColorPaletteMap.GetPalette(ColorPaletteMap.SwapType.AabayxSkin, s.Unit.SkinColor));
-
+        
         AllowedMainClothingTypes = new List<MainClothing>()
         {
             new AabayxTop1(),
@@ -78,7 +79,10 @@ class Aabayx : DefaultRaceData
             new AabayxFacePaint6(),
             new AabayxFacePaint7(),
         };
+        AllowedClothingHatTypes = new List<ClothingAccessory>();
     }
+
+    internal override int BreastSizes => 1;
 
     internal override void RandomCustom(Unit unit)
     {
