7418c43b2b8634545b60ee25cefbba330c20eefb
Robytenyx <118895579+Robytenyx@users.noreply.github.com>
2023-09-07 22:22:44 -0700

    Version bump

--- Assets/Scripts/Core/State.cs
+++ Assets/Scripts/Core/State.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 public static class State
 {
     static int saveErrors = 0;
-    public const string Version = "42";
+    public const string Version = "42A";
     public static World World;
     public static Rand Rand = new Rand();
     public static NameGenerator NameGen;
