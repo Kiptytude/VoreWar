e90a9868340a569d3fbf6985c8e313ee3cfb39c1
Flame Valxsarion <136280114+Flame-Valxsarion@users.noreply.github.com>
2024-08-10 03:15:28 -0600

    army.Side corrected to 31
    There should be 32 empires, so 31 if #0 is included, which I believe it is.  Idk, let's see what happens

--- Assets/Scripts/Modes/StrategyMode.cs
+++ Assets/Scripts/Modes/StrategyMode.cs
@@ -378,7 +378,7 @@ public class StrategyMode : SceneBase
                 {
                     armiesToReassign.Add(army);
                 }
-                if (army.Side < 32)
+                if (army.Side < 31)
                 {
                     if (army.BannerStyle > (int)BannerTypes.VoreWar && CustomBannerTest.Sprites[army.BannerStyle - 23] != null)
                     {
